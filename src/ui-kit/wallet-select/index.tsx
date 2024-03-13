e2398f07b3a3ffcb3e8a3beee7a7db684cbd8a93
Daniil <daniil@MacBook-Air-Daniil.local>
2024-03-13 09:46:32 +0200

    added custom react-select

---
+++ src/ui-kit/wallet-select/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Select, { Props, GroupBase } from 'react-select';
+import { CustomOption, CustomSingleValue, stylesSelect } from './wallet-select.styles';
+
+interface ICustomProps {
+  icon?: JSX.Element;
+  value?: string;
+  label?: string;
+  id?: string;
+}
+
+type SelectProps<
+  Option,
+  IsMulti extends boolean = false,
+  Group extends GroupBase<Option> = GroupBase<Option>,
+> = Props<Option, IsMulti, Group> & ICustomProps;
+
+export const WalletSelect = <
+  Option,
+  IsMulti extends boolean = false,
+  Group extends GroupBase<Option> = GroupBase<Option>,
+>(
+  props: SelectProps<Option, IsMulti, Group>
+) => {
+  //   const id = props.id || Math.random().toString();
+
+  const components = {
+    Option: CustomOption,
+    SingleValue: CustomSingleValue,
+    IndicatorSeparator: () => null,
+  };
+
+  return (
+    <Select
+      isClearable={false}
+      isSearchable={false}
+      {...props}
+      components={components}
+      styles={stylesSelect}
+    />
+  );
+};
---
+++ src/ui-kit/wallet-select/wallet-select.styles.tsx
@@ -0,0 +1,97 @@
+import { themeMuiBase } from '@/assets/styles/theme-mui';
+import { Box } from '@mui/system';
+import { components, CSSObjectWithLabel, OptionProps, SingleValueProps } from 'react-select';
+
+const { Option, SingleValue } = components;
+
+// TODO type any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const CustomOption = (props: OptionProps<any>) => (
+  <Option {...props}>
+    <Box display={'flex'} alignItems={'center'} gap={'8px'}>
+      {props.data.icon}
+      {props.data.label}
+    </Box>
+  </Option>
+);
+
+// TODO type any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const CustomSingleValue = (props: SingleValueProps<any>) => (
+  <SingleValue {...props}>
+    <Box display={'flex'} alignItems={'center'} gap={'8px'}>
+      {props.data.icon}
+      {props.data.label}
+    </Box>
+  </SingleValue>
+);
+
+export const stylesSelect = {
+  container: (base: CSSObjectWithLabel) => ({
+    ...base,
+    fontSize: '17px',
+    fontWeight: 600,
+
+    '&:focus-visible': {
+      borderColor: themeMuiBase.palette.black,
+    },
+  }),
+
+  dropdownIndicator: (
+    base: CSSObjectWithLabel,
+    { selectProps: { menuIsOpen } }: { selectProps: { menuIsOpen: boolean } }
+  ) => ({
+    ...base,
+    transition: 'all 0.3s',
+    color: themeMuiBase.palette.black,
+    transform: menuIsOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+  }),
+
+  control: (base: CSSObjectWithLabel) => ({
+    ...base,
+    borderRadius: '28px',
+    height: '52px',
+    padding: '0 20px',
+    borderWidth: '2px',
+    borderColor: themeMuiBase.palette.black,
+    boxShadow: 'none',
+
+    '&:hover': {
+      borderColor: themeMuiBase.palette.black,
+    },
+  }),
+
+  menu: (base: CSSObjectWithLabel) => ({
+    ...base,
+    borderRadius: '8px',
+  }),
+  menuList: (base: CSSObjectWithLabel) => ({
+    ...base,
+    borderRadius: '8px',
+  }),
+
+  option: (base: CSSObjectWithLabel, { isSelected }: { isSelected: boolean }) => ({
+    ...base,
+    display: 'flex',
+    height: '52px',
+    padding: '0 20px',
+    backgroundColor: isSelected ? themeMuiBase.palette.tetriaryDark : 'transparent',
+
+    '&:hover': {
+      backgroundColor: isSelected
+        ? themeMuiBase.palette.tetriaryDark
+        : themeMuiBase.palette.lightGrey,
+    },
+
+    '&:active': {
+      backgroundColor: isSelected ? themeMuiBase.palette.tetriaryDark : 'transparent',
+    },
+  }),
+
+  valueContainer: (base: CSSObjectWithLabel) => ({
+    ...base,
+    display: 'flex',
+    alignItems: 'center',
+    borderRadius: '28px',
+  }),
+};
