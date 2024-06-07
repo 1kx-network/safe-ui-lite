a70d05ee9cf15ea6d0374f56ac8a2e9f55965bd2
Valentyn <valentin.vad.sh@gmail.com>
2024-06-07 20:30:56 +0300

    fix: build

--- src/app/home.styles.ts
+++ src/app/home.styles.ts
@@ -1,8 +1,29 @@
 import { styled } from '@mui/system';
 
+import IconPlus from '@/assets/svg/plus.svg';
+
 export const WrapperStyled = styled('div')`
   display: flex;
   flex-direction: column;
   margin-top: 5rem;
   width: 100%;
+`;
+
+export const IconPlusStyled = styled(IconPlus)(
+  ({ theme }) => `
+    width: 18px;
+    min-width: 18px;
+    height: 18px;
+    color: ${theme.palette.main};
+    margin-right: ${theme.spacing(2)};
+  `
+);
+
+export const ItemStepPaperStyled = styled('div')`
+  display: flex;
+  width: 50%;
+
+  @media (max-width: 1060px) {
+    width: 100%;
+  }
 `;
